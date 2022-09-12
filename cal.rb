019652eef89bfbea16b0e3f2b6e4a9c9b0f6c819
Ezexiel <ezexiel.valdez@gmail.com>
2022-09-12 18:43:44 +0100

    code clean up and minor bug fix

--- cal.rb
+++ cal.rb
@@ -1,33 +1,27 @@
 def cal
   # print Month Year
-    # full name of month
-    # full year (4 digits)
+  # - full name of month
+  # - full year (4 digits)
+
   # print row of name of days (Sunday to Saturday)
-    # each days are only 2 characters
+  # - each days are only 2 characters
+
   # print days of the month
-    # 5 (to 6?) rows
-    # each column must align with the correct name of day
+  # - 5 (to 6?) rows
+  # - each column must align with the correct name of day
 
   today = Time.now
 
-  this_month_str = today.strftime("%B")
-  this_year_str = today.strftime("%Y")
   first_day_of_month = Time.new(today.year, today.month, 1)
   last_day_of_month = Time.new(today.year, today.month + 1, 1) - 1
 
-  # <inst of Time>.wday returns the name of day as integer from 0
-  # name_of_days = %w[Su Mo Tu We Th Fr Sa]
-  
-  puts "  #{this_month_str} #{this_year_str}"
+  puts "  #{today.strftime('%B')} #{today.strftime('%Y')}"
   puts 'Su Mo Tu We Th Fr Sa'
-  # puts "Date : #{Time.now}"
 
   # as we are iterating through the days to display, this tells us what day we're on
   iday = 1
-  first_day_of_month.wday
-  row_days = []
 
-  while iday < last_day_of_month.day
+  while iday <= last_day_of_month.day
     row_days = []
     (0..6).each do |wday|
       # Need to look for the first day that matches the day of week
@@ -38,6 +32,7 @@ def cal
         else
           row_days << '  '
         end
+      # If it is over the last day of the month, then we stop adding anything
       elsif iday > last_day_of_month.day
         break
       else
@@ -49,8 +44,10 @@ def cal
         iday += 1
       end
     end
+    # Display the week's days
     puts row_days.join(' ')
   end
 end
 
+# Execute program
 cal
